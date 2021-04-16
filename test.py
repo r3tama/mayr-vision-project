77a360d5e1b206373aa467eeb32520b1cbb7af1a
OutboardTech76 <fme11@protonmail.com>
2021-04-16 20:41:49 +0200

    Updated

--- test.py
+++ test.py
@@ -10,19 +10,23 @@ ImageSeg = NDArray[(Any, Any, 3), Int]
 
 
 
-def loadCsvFile(filename: Str): -> List[Image, ImageSeg]
-   data = []
-   lbl = []
-   with open(filename) as csvfile:
-       reader = csv.DictReader(csvfile, delimiter=";")
-       for row in reader:
-           try:
-               auxData = cv2.imread(row["data"])
-               auxLbl = cv2.imread(row["label"])
-               if auxData is not None:
-                   data.append(auxData)
+def loadCsvFile(filename: Str) -> List[Image, ImageSeg]:
+    data = []
+    lbl = []
+    with open(filename) as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=";")
+        for row in reader:
+            try:
+                auxData = cv2.imread(row["data"])
+                auxLbl = cv2.imread(row["label"])
+                if auxData is not None:
+                    data.append(auxData)
                 if auxLbl is not None:
                     lbl.append(auxLbl)
             except ValueError:
                 error = 1
     return data, lbl
+
+
+if __name__ == "__main__":
+    data, lbl = loadCsvFile('img.csv')
