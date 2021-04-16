6066c2df7ebd1a3b96caec7adbf5c620c993718d
rlkandela <rlkandela@gmail.com>
2021-04-16 23:37:14 +0200

    refactored the data load and changed the dataset load to only load the train split and return the test split

--- test.py
+++ test.py
@@ -1,16 +1,57 @@
+"""
+Module to tinker with models, data loading and trainings
+"""
 import cv2
 import numpy as np
 import csv
-from typing import List, Any, Tuple
+import sys
+from typing import List, Any, Tuple,Dict
 from nptyping import NDArray
+from sklearn.model_selection import train_test_split
 
 
+DataSources = Dict[str,str]
 Image = NDArray[(Any, Any, 3), int]
 ImageSeg = NDArray[(Any, Any, 3), int]
 
 
+def loadFromDataSources(d_list: List[DataSources]) -> Tuple[List[Image], List[ImageSeg]]:
+    """
+    Function to load the original images and ground truth images from the paths
+    given in the DataSources
+    Input:
+        List of DataSources
+    Returns:
+        A tuple with a list of original images and a list of ground truth images
+    """
+    if len(d_list) == 0:
+        raise ValueError("Param is empty")
+    if not isinstance(d_list[0], dict):
+        raise TypeError("Param is not a dictionary list")
+    data = []
+    lbl = []
+    count = 0
+    for row in d_list:
+        if not "data" in row.keys() or not "label" in row.keys():
+            raise ValueError("Param dictionaries do not contain the desired keys")
+        try:
+            count += 1
+            if count % 15 == 0:
+                print(count)
+            auxData = cv2.imread(row["data"])
+            auxLbl = cv2.imread(row["label"])
+            auxData = cv2.resize(auxData, (480,720))
+            auxLbl = cv2.resize(auxLbl, (480,720))
+            if auxData is not None:
+                data.append(auxData)
+            if auxLbl is not None:
+                lbl.append(auxLbl)
+        except ValueError:
+            sys.stderr.write("Could not load an image")
+    return data, lbl
 
-def loadCsvFile(filename: str) -> Tuple[List[Image], List[ImageSeg]]:
+
+def loadCsvFile(filename: str) -> Tuple[List[Image], List[ImageSeg],List[DataSources]]:
     """
         Function to load original images and ground truth images from .csv file
         Input:
@@ -20,28 +61,13 @@ def loadCsvFile(filename: str) -> Tuple[List[Image], List[ImageSeg]]:
     """
     if not isinstance(filename, str):
         raise TypeError("Name is not a string")
-        return 
-    data = []
-    lbl = []
-    count=0
     with open(filename) as csvfile:
-        reader = csv.DictReader(csvfile, delimiter=";")
-        for row in reader:
-            try:
-                auxData = cv2.imread(row["data"])
-                auxLbl = cv2.imread(row["label"])
-                auxData = cv2.resize(auxData, (480,720))
-                auxLbl = cv2.resize(auxLbl, (480,720))
-                count +=1
-                if auxData is not None:
-                    data.append(auxData)
-                if auxLbl is not None:
-                    lbl.append(auxLbl)
-                    print(count)
-            except ValueError:
-                error = 1
-    return data, lbl
+        data_storage = list(csv.DictReader(csvfile, delimiter=";"))
+        train_dict, test_dict = train_test_split(data_storage,test_size=0.3,train_size=0.7,random_state=69)
+        data,lbl = loadFromDataSources(train_dict)
+        return data,lbl,test_dict
+
 
 
 if __name__ == "__main__":
-    data, lbl = loadCsvFile('img.csv')
+    data, lbl,test_dict = loadCsvFile('img.csv')
