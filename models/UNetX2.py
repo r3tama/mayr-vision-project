1d415ac8337d9cdea873844821d8a08bcfcc9124
OutboardTech76 <fme11@protonmail.com>
2021-04-17 06:32:26 +0200

    Removed all loops. Not working

--- models/UNetX2.py
+++ models/UNetX2.py
@@ -23,7 +23,7 @@ class UNetXception(_keras.Model):
         if not isinstance(nClasses, int):
             raise TypeError("nClasses mus be of type int")
         if len(nFilters) != 8:
-            raise ValueErrorr("nFilters must have 8 elements")
+            raise ValueError("nFilters must have 8 elements")
              
         self.nFilters = nFilters
         self.nClasses = nClasses
@@ -36,31 +36,18 @@ class UNetXception(_keras.Model):
         self.batchNorm1 = _keras.layers.BatchNormalization()
         self.act1 = _keras.layers.Activation("relu")
 
-        # Create pooling
-        self.pooling = _keras.layers.MaxPooling2D(pool_size=3,
-                                                  strides=2,
-                                                  padding="same")
-        # Create upsampling
-        self.upsampling = _keras.layers.UpSampling2D(size=2)
 
         # Initialize arrays for layers with different filter size
-        self.sepConv = _np.empty(3, dtype=SepConv2DKeras)
-        self.sepAct = _np.empty(3, dtype=ActivationKeras)
-        self.sepBatch = _np.empty(3, dtype=BatchNormKeras)
-        self.residualDown = _np.empty(3, dtype=Conv2DKeras)
-        self.transConv = _np.empty(4, dtype=TranspConv2DKeras)
-        self.transAct = _np.empty(4, dtype=ActivationKeras)
-        self.transBatch = _np.empty(4, dtype=BatchNormKeras)
-        self.residualUp = _np.empty(4, dtype=Conv2DKeras)
          
-        self.sepConv = _np.empty(3, dtype=SepConv2DKeras)
-        self.sepAct = _np.empty(3, dtype=ActivationKeras)
-        self.sepBatch = _np.empty(3, dtype=BatchNormKeras)
-        self.residualDown = _np.empty(3, dtype=Conv2DKeras)
-        self.transConv = _np.empty(4, dtype=TranspConv2DKeras)
-        self.transAct = _np.empty(4, dtype=ActivationKeras)
-        self.transBatch = _np.empty(4, dtype=BatchNormKeras)
-        self.residualUp = _np.empty(4, dtype=Conv2DKeras)
+        # self.sepConv = _np.empty(3, dtype=SepConv2DKeras)
+        # self.sepAct = _np.empty(3, dtype=ActivationKeras)
+        # self.sepBatch = _np.empty(3, dtype=BatchNormKeras)
+        # self.residualDown = _np.empty(3, dtype=Conv2DKeras)
+        # self.transConv = _np.empty(4, dtype=TranspConv2DKeras)
+        # self.transAct = _np.empty(4, dtype=ActivationKeras)
+        # self.transBatch = _np.empty(4, dtype=BatchNormKeras)
+        # self.residualUp = _np.empty(4, dtype=Conv2DKeras)
+         
         # self.sepConv = _np.empty(len(self.nFilters), dtype=int)
         # self.sepAct = _np.empty(len(self.nFilters), dtype=int)
         # self.sepBatch = _np.empty(len(self.nFilters), dtype=int)
@@ -70,7 +57,7 @@ class UNetXception(_keras.Model):
         # self.transBatch = _np.empty(len(self.nFilters), dtype=int)
         # self.residualUp - _np.empty(len(self.nFilter), dtype=int)
 
-        count = 0
+        # First half of the network. Downsampling convolutions
 
         self.sepConv1 = _keras.layers.SeparableConv2D(filters=self.nFilters[1],
                                                             kernel_size=3,
@@ -81,7 +68,30 @@ class UNetXception(_keras.Model):
                                                     kernel_size=1,
                                                     padding="same",
                                                     strides=2)
-        # First half of the network. Downsampling convolutions
+         
+        # Create pooling
+        self.pooling = _keras.layers.MaxPooling2D(pool_size=3,
+                                                  strides=2,
+                                                  padding="same")
+         
+        self.sepConv2 = _keras.layers.SeparableConv2D(filters=self.nFilters[2],
+                                                            kernel_size=3,
+                                                            padding="same")
+        self.sepAct2 = _keras.layers.Activation("relu")
+        self.sepBatch2 = _keras.layers.BatchNormalization()
+        self.residualDown2 = _keras.layers.Conv2D(filters=self.nFilters[2],
+                                                    kernel_size=1,
+                                                    padding="same",
+                                                    strides=2)
+        self.sepConv3 = _keras.layers.SeparableConv2D(filters=self.nFilters[3],
+                                                            kernel_size=3,
+                                                            padding="same")
+        self.sepAct3 = _keras.layers.Activation("relu")
+        self.sepBatch3 = _keras.layers.BatchNormalization()
+        self.residualDown3 = _keras.layers.Conv2D(filters=self.nFilters[3],
+                                                    kernel_size=1,
+                                                    padding="same",
+                                                    strides=2)
         # for filt in self.nFilters[1:4]:
             # self.sepConv[count] = _keras.layers.SeparableConv2D(filters=filt,
                                                                 # kernel_size=3,
@@ -94,18 +104,55 @@ class UNetXception(_keras.Model):
                                                         # strides=2)
             # count += 1
 
-        count = 0
-        # Second half of the network. Upsampling convolutions
-        for filt in self.nFilters[4:]:
-            self.transConv[count] = _keras.layers.Conv2DTranspose(filters=filt,
-                                                       kernel_size=3,
-                                                       padding="same")
-            self.transAct[count] = _keras.layers.Activation("relu")
-            self.transBatch[count] = _keras.layers.BatchNormalization()
-            self.residualUp[count] = _keras.layers.Conv2D(filters=filt,
-                                                          kernel_size=1,
-                                                          padding="same")
-            count += 1
+        self.transConv1 = _keras.layers.Conv2DTranspose(filters=self.nFilters[4],
+                                                   kernel_size=3,
+                                                   padding="same")
+        self.transAct1 = _keras.layers.Activation("relu")
+        self.transBatch1 = _keras.layers.BatchNormalization()
+        # Create upsampling
+        self.upsampling = _keras.layers.UpSampling2D(size=2)
+        self.residualUp1 = _keras.layers.Conv2D(filters=self.nFilters[4],
+                                                      kernel_size=1,
+                                                      padding="same")
+         
+        self.transConv2 = _keras.layers.Conv2DTranspose(filters=self.nFilters[5],
+                                                   kernel_size=3,
+                                                   padding="same")
+        self.transAct2 = _keras.layers.Activation("relu")
+        self.transBatch2 = _keras.layers.BatchNormalization()
+        self.residualUp2 = _keras.layers.Conv2D(filters=self.nFilters[5],
+                                                      kernel_size=1,
+                                                      padding="same")
+         
+        self.transConv3 = _keras.layers.Conv2DTranspose(filters=self.nFilters[6],
+                                                   kernel_size=3,
+                                                   padding="same")
+        self.transAct3 = _keras.layers.Activation("relu")
+        self.transBatch3 = _keras.layers.BatchNormalization()
+        self.residualUp3 = _keras.layers.Conv2D(filters=self.nFilters[6],
+                                                      kernel_size=1,
+                                                      padding="same")
+         
+        self.transConv4 = _keras.layers.Conv2DTranspose(filters=self.nFilters[7],
+                                                   kernel_size=3,
+                                                   padding="same")
+        self.transAct4 = _keras.layers.Activation("relu")
+        self.transBatch4 = _keras.layers.BatchNormalization()
+        self.residualUp4 = _keras.layers.Conv2D(filters=self.nFilters[7],
+                                                      kernel_size=1,
+                                                      padding="same")
+        # count = 0
+        # # Second half of the network. Upsampling convolutions
+        # for filt in self.nFilters[4:]:
+            # self.transConv[count] = _keras.layers.Conv2DTranspose(filters=filt,
+                                                       # kernel_size=3,
+                                                       # padding="same")
+            # self.transAct[count] = _keras.layers.Activation("relu")
+            # self.transBatch[count] = _keras.layers.BatchNormalization()
+            # self.residualUp[count] = _keras.layers.Conv2D(filters=filt,
+                                                          # kernel_size=1,
+                                                          # padding="same")
+            # count += 1
 
         # Create final layer with classes number as size
         self.out = _keras.layers.Conv2D(filters=self.nClasses,
@@ -122,12 +169,84 @@ class UNetXception(_keras.Model):
 
         # Save layer for residual movement
         prevLayer = x
-        x = self.pooling(x)
-        x = self.upsampling(x)
+        # for _ in range(2):
+        x = self.sepAct1(x)
         x = self.sepConv1(x)
         x = self.sepBatch1(x)
+         
         x = self.sepAct1(x)
-
+        x = self.sepConv1(x)
+        # x = self.sepBatch1(x)
+         
+        # x = self.pooling(x)
+        # residual = self.residualDown1(prevLayer)
+        # x = _keras.layers.add([x, residual])
+        # prevLayer = x
+             
+        # for _ in range(2):
+        # x = self.sepConv2(x)
+        # x = self.sepBatch2(x)
+        # x = self.sepAct2(x)
+         
+        # x = self.sepConv2(x)
+        # x = self.sepBatch2(x)
+        # x = self.sepAct2(x)
+         
+        # x = self.pooling(x)
+        # residual = self.residualDown2(prevLayer)
+        # x = _keras.layers.add([x, residual])
+        # prevLayer = x
+         
+        # for _ in range(2):
+            # x = self.sepConv3(x)
+            # x = self.sepBatch3(x)
+            # x = self.sepAct3(x)
+        # x = self.pooling(x)
+        # residual = self.residualDown3(prevLayer)
+        # x = _keras.layers.add([x, residual])
+        # prevLayer = x
+         
+        # for _ in range(2):
+            # x = self.transConv1(x)
+            # x = self.transBatch1(x)
+            # x = self.transAct1(x)
+        # x = self.upsampling(x)
+        # residual = self.upsampling(prevLayer)
+        # residual = self.residualUp1(residual)
+        # x = _keras.layers.add([x, residual])
+        # prevLayer = x
+         
+        # for _ in range(2):
+            # x = self.transConv2(x)
+            # x = self.transBatch2(x)
+            # x = self.transAct2(x)
+        # x = self.upsampling(x)
+        # residual = self.upsampling(prevLayer)
+        # residual = self.residualUp2(residual)
+        # x = _keras.layers.add([x, residual])
+        # prevLayer = x
+         
+        # for _ in range(2):
+            # x = self.transConv3(x)
+            # x = self.transBatch3(x)
+            # x = self.transAct3(x)
+        # x = self.upsampling(x)
+        # residual = self.upsampling(prevLayer)
+        # residual = self.residualUp3(residual)
+        # x = _keras.layers.add([x, residual])
+        # prevLayer = x
+         
+        # for _ in range(2):
+            # x = self.transConv4(x)
+            # x = self.transBatch4(x)
+            # x = self.transAct4(x)
+        # x = self.upsampling(x)
+        # residual = self.upsampling(prevLayer)
+        # residual = self.residualUp4(residual)
+        # x = _keras.layers.add([x, residual])
+        # prevLayer = x
+         
+         
         # # Convolutional downsampling (filters number = 3)
         # for count in range(3):
             # for _ in range(2):
