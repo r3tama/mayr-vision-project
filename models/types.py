589d1a37fa6d02a879a1f685754a090cfeb79431
OutboardTech76 <fme11@protonmail.com>
2021-04-17 06:11:43 +0200

    Added keras typing

--- models/types.py
+++ models/types.py
@@ -2,8 +2,11 @@
 Module for unifying the typing system
 """
 from typing import Any as _Any
+from tensorflow import keras as _keras
 
-__all__=["ColorImageSize","ImageSize"]
+__all__=["ColorImageSize","ImageSize","SepConv2DKeras",
+         "BatchNormKeras", "Conv2DKeras", "ActivationKeras",
+         "TranspConv2DKeras", "UpsampligKeras", "PoolingKeras"]
 
 ColorImageSize = (_Any,_Any,3)
 """
@@ -16,3 +19,14 @@ ImageSize = (_Any,_Any)
 One-chanel image shaping
 ImageSize, aka (Any, Any)
 """
+
+"""
+Objects for keras typing
+"""
+SepConv2DKeras = _keras.layers.SeparableConv2D
+BatchNormKeras = _keras.layers.BatchNormalization
+Conv2DKeras = _keras.layers.Conv2D
+ActivationKeras  = _keras.layers.Activation
+TranspConv2DKeras = _keras.layers.Conv2DTranspose
+UpsampligKeras = _keras.layers.UpSampling2D
+PoolingKeras = _keras.layers.MaxPooling2D
